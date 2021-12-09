47b4e26b0b1370b8c1a45712005ffa8a7703c5d1
AndresCV14 <94935280+AndresCV14@users.noreply.github.com>
2021-12-09 13:58:47 -0500

    Update netsize.sh
    en la línea 8 se eliminó "$1" y se agregó "n1.txt"

--- Tarea 1/netsize.sh
+++ Tarea 1/netsize.sh
@@ -1,10 +1,8 @@
-#Resolución del primer apartado
-
-#!/bin/bash
+#!/bin/bash/../data/Saavedra2013
 
 echo "../data/Saavedra2013/n1.txt"
 echo n1.txt
 echo "Número de filas:"
 cat n1.txt | wc -l
 echo "Número de columnas:"
-head -n 1 $1 | tr -d ' ' | tr -d '\n' | wc -c
+head -n 1 n1.txt | tr -d ' ' | tr -d '\n' | wc -c
