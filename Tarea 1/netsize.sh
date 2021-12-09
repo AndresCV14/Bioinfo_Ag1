d2ec1c41444bff00587830ac00f8fccea12111f9
AndresCV14 <94935280+AndresCV14@users.noreply.github.com>
2021-12-09 12:58:02 -0500

    Update netsize.sh
    Se agregó la primera opción de script

--- Tarea 1/netsize.sh
+++ Tarea 1/netsize.sh
@@ -1,1 +1,10 @@
+# Resolución del primer apartado
 
+## /Desktop/CSB/unix/data/Saacedra2013
+
+echo "../data/Saavedra2013/n1.txt"
+echo n1.txt
+echo "Número de filas:"
+cat n1.txt | wc -l
+echo "Número de columnas:"
+head -n 1 $1 | tr -d ' ' | tr -d '\n' | wc -c
