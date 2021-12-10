8b171599d13411fd2bbc10c7821344bc68f31cb9
AndresCV14 <94935280+AndresCV14@users.noreply.github.com>
2021-12-10 09:05:18 -0500

    Update netsize.sh
    Se cambio el script

--- Tarea 1/netsize.sh
+++ Tarea 1/netsize.sh
@@ -1,8 +1,4 @@
-#!/bin/bash/../data/Saavedra2013
+#!/User/Andy/../data/Saavedra2013
 
-echo "../data/Saavedra2013/n1.txt"
-echo n1.txt
-echo "Número de filas:"
-cat n1.txt | wc -l
-echo "Número de columnas:"
-head -n 1 n1.txt | tr -d ' ' | tr -d '\n' | wc -c
+echo "Filename:../data/Saavedra2013/n1.txt"
+echo "Número de filas:" ; cat n1.txt | wc -l ; echo "Número de columnas:" ; head -n 1 n1.txt | tr -d ' ' | tr -d '\n' | wc -c ; echo "Fin script"
