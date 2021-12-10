ffa1ab1c8968d3eec960d842f26a838079f500a2
AndresCV14 <94935280+AndresCV14@users.noreply.github.com>
2021-12-10 10:03:58 -0500

    Update netsize_all.sh
    Se añadió el script

--- Tarea 1/netsize_all.sh
+++ Tarea 1/netsize_all.sh
@@ -1,5 +1,7 @@
 #!/User/Andy/../data/Saavedra2013
 # Se debe estar posicionado en el directorio Saavedra2013
 # Se debe ejecutar el siguiente script
+for file in $(ls *.txt); do wc -l $file; head -n1 $file | grep -o ' ' | wc -l; done
 
+# El script regresará un conteo por cada archivo de lineas y columnas, que se encuentren en la carpeta
 
