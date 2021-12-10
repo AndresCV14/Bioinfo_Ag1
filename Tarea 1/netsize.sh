10a62fe44c6a9daafa5ff227a62aa8da40c184a8
AndresCV14 <94935280+AndresCV14@users.noreply.github.com>
2021-12-10 09:10:34 -0500

    Update netsize.sh
    Se agregaron los resultados obtenidos con el script planteado

--- Tarea 1/netsize.sh
+++ Tarea 1/netsize.sh
@@ -2,3 +2,7 @@
 
 echo "Filename:../data/Saavedra2013/n1.txt"
 echo "Número de filas:" ; cat n1.txt | wc -l ; echo "Número de columnas:" ; head -n 1 n1.txt | tr -d ' ' | tr -d '\n' | wc -c ; echo "Fin script"
+
+# Resultados
+# Número de filas: 97
+# Número de columans: 80
